bae5dd86a0355fd034478cc1b4308d5ac87c7825
Leo <no@no.yes>
2024-01-25 19:49:34 +0100

    Added blinking LED and a 3rd sensor with a 3rd sound

--- script/script.ino
+++ script/script.ino
@@ -10,22 +10,28 @@
 //sensor1 -> handsensor
 //sensor2 -> präsenzsensor
 const int sensor1_pin = A0; // connect IR sensor module to Arduino pin A0
-const int sensor2_pin = A1; // praesense sensor module to Arduino pin A1
+const int sensor2_pin = A1; // praesense sensor module for hands to Arduino pin A1
+const int sensor3_pin = A2; // praesense sensor module for tamper detection to Arduino pin A2
 const int relay1_pin = 2; // relay 1 on pin D2 soap dispenser
 const int relay2_pin = 3; // relay 2 on pin D3 kookoo bird
+const int LED1_pin = A3; // LED
 
 const int DFPlayer_arduinoRX = 10; // pin D10 -> RX on arduino TX on DFPlayer
 const int DFPlayer_arduinoTX = 11; // pin D11 -> TX on arduino RX on DFPlayer
 
 const int sound1_playFor = 5000; //how long should sound 1 be played?
 const int sound2_playFor = 5000; //how long should sound 2 be played?
+const int sound3_playFor = 5000; //how long should sound 3 be played?
 const int relay1_switchFor = 300; // how long should the relay1 be switched?
 const int relay2_switchFor = 300; // how long should the relay2 be switched?
 
 const int sound1_backoff = 10000 + relay2_switchFor; // how long should the sound 1 not be played after it has been played
 const int sound2_backoff = 30000; // how long should the sound 2 not be played after it has been played
+const int sound3_backoff = 30000; // how long should the sound 3 not be played after it has been played
 const int relay1_backoff = 2000; // how long should the relay not be switched after it has been through a switch cycle
 const int relay2_backoff = 10000 + sound1_playFor; // how long should the relay not be switched after it has been through a switch cycle
+
+const int ledBlinkInterval = 1000;
 
 const int volume = 10; // sound volume 0-30 
 
@@ -40,6 +46,8 @@ unsigned long sound1_lastPlayStop = 1;
 unsigned long sound2_lastPlayStart = 0;
 unsigned long sound2_lastPlayStop = 1;
 
+unsigned long sound3_lastPlayStart = 0;
+unsigned long sound3_lastPlayStop = 1;
 
 unsigned long relay1_lastTriggerOn = 0;
 unsigned long relay1_lastTriggerOff = 1;
@@ -47,8 +55,12 @@ unsigned long relay1_lastTriggerOff = 1;
 unsigned long relay2_lastTriggerOn = 0;
 unsigned long relay2_lastTriggerOff = 1;
 
+unsigned long led1_lastTriggerOn = 0;
+unsigned long led1_lastTriggerOff = 1;
+
 bool sensor1_lastState = false;
 bool sensor2_lastState = false;
+bool sensor3_lastState = false;
 
 static unsigned long currentTime;
 
@@ -61,19 +73,25 @@ bool isCurrentlyInAction(unsigned long actionStart, unsigned long actionEnd);
 bool isCurrentlyNotInAction(unsigned long actionStart, unsigned long actionEnd);
 void switchSensor1RelatedStuffOn();
 void switchSensor2RelatedStuffOn();
+void switchSensor3RelatedStuffOn();
 void handleRelay1SwitchOn();
 void handleRelay1SwitchOff();
 void handleSound1Start();
 void handleSound1Stop();
 void handleSound2Start();
 void handleSound2Stop();
+void handleSound3Start();
+void handleSound3Stop();
+void ledSwitch();
 
 void setup() {
   pinMode(sensor1_pin, INPUT);
   pinMode(sensor2_pin, INPUT);
+  pinMode(sensor3_pin, INPUT);
   pinMode(relay1_pin, OUTPUT);
   pinMode(relay2_pin, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED1_pin, OUTPUT);
 
   digitalWrite(relay1_pin, HIGH);
   digitalWrite(relay2_pin, HIGH);
@@ -87,14 +105,18 @@ void setup() {
   delay(500);
 }
 
+
 void loop() {
   delay(20);
   // Sensor-Zustand überprüfen
   bool sensor1_isOn = !digitalRead(sensor1_pin);
   bool sensor2_isOn = !digitalRead(sensor2_pin);
+  bool sensor3_isOn = !digitalRead(sensor3_pin); 
 
   //display sensor1 state with buldin led
   digitalWrite(LED_BUILTIN, sensor1_isOn);
+
+  ledSwitch();
 
   currentTime = millis() + 30000; //give it a little headstart / offset
 
@@ -116,10 +138,20 @@ void loop() {
     sensor2_lastState = false;
   }
 
+  if(sensor3_isOn && sensor3_lastState == false) {
+    Serial.print("Sensor_3 is on ");
+    Serial.println(currentTime);
+    switchSensor3RelatedStuffOn();
+    sensor3_lastState = true;
+  } else if(!sensor3_isOn && sensor3_lastState == true) {
+    sensor3_lastState = false;
+  }
+
   handleRelay1SwitchOff();
   handleRelay2SwitchOff();
   handleSound1Stop();
   handleSound2Stop();
+  handleSound3Stop();
 }
 
 void switchSensor1RelatedStuffOn() {
@@ -130,6 +162,10 @@ void switchSensor1RelatedStuffOn() {
 
 void switchSensor2RelatedStuffOn() {
   handleSound2Start();
+}
+
+void switchSensor3RelatedStuffOn() {
+  handleSound3Start();
 }
 
 // Die folgenden Methoden funktionieren alle gleich:
@@ -228,6 +264,45 @@ void handleSound2Stop() {
       Serial.println("stop sound2");
       mp3Player.stop();
       sound2_lastPlayStop = currentTime;
+    }
+  }
+}
+
+void handleSound3Start() {
+  if(isCurrentlyNotInAction(sound3_lastPlayStart, sound3_lastPlayStop))  {
+    //sound can be started
+    if(checkTimeOver(sound3_lastPlayStop, sound2_backoff)) {
+      Serial.println("start sound3");
+      mp3Player.play(3);
+      sound3_lastPlayStart = currentTime;
+    }
+  }
+}
+
+void handleSound3Stop() {
+  if(isCurrentlyInAction(sound3_lastPlayStart, sound3_lastPlayStop)) {
+    //sound can be stopped
+    if(checkTimeOver(sound3_lastPlayStart, sound3_playFor)) {
+      Serial.println("stop sound3");
+      mp3Player.stop();
+      sound3_lastPlayStop = currentTime;
+    }
+  }
+}
+
+
+void ledSwitch() {
+  if(isCurrentlyInAction(led1_lastTriggerOn,led1_lastTriggerOff)) {
+    if(checkTimeOver(led1_lastTriggerOn, ledBlinkInterval)) {
+      //Serial.println("switch led off");
+      digitalWrite(LED1_pin, LOW); 
+      led1_lastTriggerOff = currentTime;
+    }
+  } else {
+    if(checkTimeOver(led1_lastTriggerOff, ledBlinkInterval)) {
+      //Serial.println("switch led on");
+      digitalWrite(LED1_pin, HIGH); 
+      led1_lastTriggerOn = currentTime;
     }
   }
 }
